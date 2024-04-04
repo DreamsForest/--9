94dad76e1c6921946d0553554b32933bb7921b9f
DreamsForest <muhinv451@gmail.com>
2024-04-04 19:16:49 +0300

    2

--- code/main/main/main.cpp
+++ code/main/main/main.cpp
@@ -4,14 +4,14 @@
 using namespace std;
 
 int main() {
-    int num1, num2, sum, difference; // добавляем переменную для хранения разности
+    int nummber1, num2, sum, difference; 
     cout << "Введите первое число: ";
-    cin >> num1;
+    cin >> number1;
     cout << "Введите второе число: ";
     cin >> num2;
-    sum = num1 + num2;
-    difference = num1 - num2; // вычисляем разность
-    cout << "Сумма: " << sum << endl; // выводим и сумму, и разность
+    sum = nummber1 + num2;
+    difference = nummber1 - num2; 
+    cout << "Сумма: " << sum << endl; 
     cout << "Разность: " << difference << endl;
 
     return 0;
