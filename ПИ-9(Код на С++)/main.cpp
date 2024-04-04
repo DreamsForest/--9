332593267741903ae940c0aaeb59f9092c9db4eb
DreamsForest <muhinv451@gmail.com>
2024-04-04 11:26:36 +0300

    Добавлен файл std.h и main.cpp с вводом двух целых чисел

---
+++ ПИ-9(Код на С++)/main.cpp
@@ -0,0 +1,13 @@
+﻿#include "sdt.h"
+#include <iostream>
+
+using namespace std;
+int main() {
+    int num1, num2;
+    cout << "Введите два целых числа: ";
+    cin >> num1;
+    cin >> num2;
+    
+
+    return 0;
+}
