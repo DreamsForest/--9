f2355c0a79aa2849391a8fa4808bc51fd725633a
DreamsForest <muhinv451@gmail.com>
2024-04-04 18:11:05 +0300

    Добавлена печать разности введенных цифр

--- code/main/main/main.cpp
+++ code/main/main/main.cpp
@@ -1,15 +1,18 @@
-﻿#include "sdt.h"
-#include <iostream>
+﻿#include <iostream>
+#include "sdt.h"
 
 using namespace std;
+
 int main() {
-    int num1, num2, summ;
+    int num1, num2, sum, difference; // добавляем переменную для хранения разности
     cout << "Введите первое число: ";
     cin >> num1;
     cout << "Введите второе число: ";
     cin >> num2;
-    summ = num1 + num2;
-    cout << summ;
+    sum = num1 + num2;
+    difference = num1 - num2; // вычисляем разность
+    cout << "Сумма: " << sum << endl; // выводим и сумму, и разность
+    cout << "Разность: " << difference << endl;
 
     return 0;
 }
