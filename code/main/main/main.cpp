5594883d0ba99890a7593128ae647a94702a1fe6
DreamsForest <muhinv451@gmail.com>
2024-04-06 18:19:40 +0300

    Добавил результат частного

--- code/main/main/main.cpp
+++ code/main/main/main.cpp
@@ -4,15 +4,16 @@
 using namespace std;
 //Коментарий
 int main() {
-    int num1, num2, sum, difference; 
+    int num1, num2, sum, difference, private_number;
     cout << "Введите первое число: ";
     cin >> num1;
     cout << "Введите второе число: ";
     cin >> num2;
     sum = num1 + num2;
-    difference = num1 - num2; 
+    difference = num1 - num2;
+    private_number = num1 \ num2;
     cout << "Сумма: " << sum << endl; 
     cout << "Разность: " << difference << endl;
-
+    cout << "Частное: " << private_number << endl;
     return 0;
 }
